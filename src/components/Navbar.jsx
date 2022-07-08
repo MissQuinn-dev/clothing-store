e026f4c3dd523bc377acc4f5fc6dd570d3f85ce7
Quinn E George <103374088+MissQuinn-dev@users.noreply.github.com>
2022-07-08 14:24:33 -0500

    Cleanup

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,15 @@
 import React from 'react';
 import { styled, alpha } from '@mui/material/styles';
-import { Link } from 'react-router-dom';
 import NavDrawer from './NavDrawer';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import Badge from '@mui/material/Badge';
-import MenuItem from '@mui/material/MenuItem';
-import Menu from '@mui/material/Menu';
-import AccountCircle from '@mui/icons-material/AccountCircle';
-import MoreIcon from '@mui/icons-material/MoreVert';
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 import Button from '@mui/material/Button';
 import ComboBox from './ComboBox';
+import Typography from '@mui/material/Typography';
 import { useGlobalContext } from '../context';
 
 const Search = styled('div')(({ theme }) => ({
@@ -32,191 +28,33 @@ const Search = styled('div')(({ theme }) => ({
   },
 }));
 
-export const Navbar = () => {
+const Navbar = () => {
   const { categoryArray } = useGlobalContext();
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
-  const isMenuOpen = Boolean(anchorEl);
-  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
-  const handleProfileMenuOpen = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleMobileMenuClose = () => {
-    setMobileMoreAnchorEl(null);
-  };
-
-  const handleMenuClose = () => {
-    setAnchorEl(null);
-    handleMobileMenuClose();
-  };
-
-  const handleMobileMenuOpen = (event) => {
-    setMobileMoreAnchorEl(event.currentTarget);
-  };
-
-  const menuId = 'primary-search-account-menu';
-  const renderMenu = (
-    <Menu
-      anchorEl={anchorEl}
-      anchorOrigin={{
-        vertical: 'top',
-        horizontal: 'right',
-      }}
-      id={menuId}
-      keepMounted
-      transformOrigin={{
-        vertical: 'top',
-        horizontal: 'right',
-      }}
-      open={isMenuOpen}
-      onClose={handleMenuClose}
-    >
-      <MenuItem>
-        <Link onClick={handleMenuClose} to="/" style={{ textDecoration: 'none' }}>
-          Home
-        </Link>
-      </MenuItem>
-      <MenuItem>
-        <Link onClick={handleMenuClose} to="/products" style={{ textDecoration: 'none' }}>
-          Products
-        </Link>
-      </MenuItem>
-      <MenuItem>
-        <Link onClick={handleMenuClose} to="/cartcontainer" style={{ textDecoration: 'none' }}>
-          Cart
-        </Link>
-      </MenuItem>
-      <MenuItem>
-        <Link onClick={handleMenuClose} to="/checkout" style={{ textDecoration: 'none' }}>
-          Check Out
-        </Link>
-      </MenuItem>
-    </Menu>
-  );
-
-  const mobileMenuId = 'primary-search-account-menu-mobile';
-  const renderMobileMenu = (
-    <Menu
-      anchorEl={mobileMoreAnchorEl}
-      anchorOrigin={{
-        vertical: 'top',
-        horizontal: 'right',
-      }}
-      id={mobileMenuId}
-      keepMounted
-      transformOrigin={{
-        vertical: 'top',
-        horizontal: 'right',
-      }}
-      open={isMobileMenuOpen}
-      onClose={handleMobileMenuClose}
-    >
-      <MenuItem>
-        <IconButton size="large" aria-label="show 10 new items" color="inherit">
-          <Badge badgeContent={10} color="error">
-            <ShoppingCartOutlinedIcon />
-          </Badge>
-        </IconButton>
-        <p>Cart</p>
-      </MenuItem>
-      <MenuItem>
-        <Link
-          m={8}
-          onClick={handleMenuClose}
-          to="/"
-          style={{ textDecoration: 'none', color: 'black' }}
-        >
-          Home
-        </Link>
-      </MenuItem>
-      <MenuItem>
-        <Link
-          onClick={handleMenuClose}
-          to="/products"
-          style={{ textDecoration: 'none', color: 'black' }}
-        >
-          Products
-        </Link>
-      </MenuItem>
-
-      <MenuItem>
-        <Link
-          onClick={handleMenuClose}
-          to="/checkout"
-          style={{ textDecoration: 'none', color: 'black' }}
-        >
-          Check Out
-        </Link>
-      </MenuItem>
-      <MenuItem>
-        <Link
-          onClick={handleMenuClose}
-          to="/cartcontainer"
-          style={{ textDecoration: 'none', color: 'black' }}
-        >
-          <IconButton size="large" aria-label="show 13 new items" color="inherit"></IconButton>
-          <Badge badgeContent={13} color="error">
-            <ShoppingCartOutlinedIcon />
-          </Badge>
-        </Link>
-      </MenuItem>
-    </Menu>
-  );
 
   return (
     <Box sx={{ flexGrow: 1 }} position="sticky" style={{ marginTop: 100 }}>
       <AppBar>
         <Toolbar>
           <NavDrawer />
-
+          <Typography variant="h5">BunNeed</Typography>
           <Search>
             <ComboBox />
           </Search>
           <Box sx={{ flexGrow: 1 }} />
           <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
             {categoryArray.map(({ category, id }) => (
-              <Button
-                key={id}
-                // onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: 'black', display: 'block' }}
-              >
+              <Button key={id} sx={{ my: 2, color: 'black', display: 'block' }}>
                 {category}
               </Button>
             ))}
-            <IconButton size="large" aria-label="show 4 new items" color="inherit">
+            <IconButton size="large" aria-label="show 13 new items" color="inherit">
               <Badge badgeContent={13} color="error">
                 <ShoppingCartOutlinedIcon />
               </Badge>
             </IconButton>
-            <IconButton
-              size="large"
-              edge="end"
-              aria-label="account of current user"
-              aria-controls={menuId}
-              aria-haspopup="true"
-              onClick={handleProfileMenuOpen}
-              color="inherit"
-            >
-              <AccountCircle />
-            </IconButton>
-          </Box>
-          <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
-            <IconButton
-              size="large"
-              aria-label="show more"
-              aria-controls={mobileMenuId}
-              aria-haspopup="true"
-              onClick={handleMobileMenuOpen}
-              color="inherit"
-            >
-              <MoreIcon />
-            </IconButton>
           </Box>
         </Toolbar>
       </AppBar>
-      {renderMobileMenu}
-      {renderMenu}
     </Box>
   );
 };
