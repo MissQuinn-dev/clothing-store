bc6cd9ce00ee748c02f511f12b2170bdd88e4163
Quinn E George <103374088+MissQuinn-dev@users.noreply.github.com>
2022-07-13 12:54:18 -0500

    removed unused CssBaseline

--- src/components/cart/CartContainer.jsx
+++ src/components/cart/CartContainer.jsx
@@ -1,46 +1,50 @@
 import React from 'react';
-import CartItem from './CartItem';
-import { useGlobalContext } from '../../context';
+import { Link } from 'react-router-dom';
+import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import CartItem from './CartItem';
+import Grid from '@mui/material/Grid';
 
+import { useGlobalContext } from '../../context';
 const CartContainer = () => {
-  const { cart, total, clearCart } = useGlobalContext();
+  const { cart } = useGlobalContext();
+
   if (cart.length === 0) {
     return (
-      <Box sx={{ width: '100%' }} style={{ marginTop: 100 }}>
-        <section style={{ marginTop: 100 }}>
-          <header>
-            <h2>your bag</h2>
-            <h4>is currently empty</h4>
-          </header>
-        </section>
-      </Box>
+      <React.Fragment>
+        <Box textAlign="center" mt={3}>
+          <Typography variant="h4" component="div">
+            Oh No!~
+          </Typography>
+          <Typography gutterBottom variant="h5" component="div">
+            You have not picked out anything yet.
+          </Typography>
+
+          <Link to="/" style={{ textDecoration: 'none' }}>
+            <Button
+              style={{ transform: 'translate(10%, -20%)' }}
+              variant="contained"
+              color="secondary"
+            >
+              Take me back home!
+            </Button>
+          </Link>
+        </Box>
+      </React.Fragment>
     );
   }
   return (
-    <Box sx={{ width: '100%' }} style={{ marginTop: 100 }}>
-      <section>
-        <header>
-          <h2>your bag</h2>
-        </header>
-        <div>
-          {cart &&
-            cart.map((item) => {
-              return <CartItem key={item.id} {...item} />;
-            })}
-        </div>
-        <footer>
-          <hr />
-          <div>
-            <h4>
-              total <span>${total}</span>
-            </h4>
-          </div>
-          <button onClick={clearCart}>clear cart</button>
-        </footer>
-      </section>
-    </Box>
+    <React.Fragment>
+      <Grid container spacing={4} justifyContent="center" alignItems="stretch">
+        {cart.map((cart, id) => {
+          return <CartItem key={id} {...cart} />;
+        })}
+      </Grid>
+      <Button variant="contained" color="secondary" onClick={() => console.log(...cart)}>
+        Test
+      </Button>
+    </React.Fragment>
   );
 };
-
 export default CartContainer;
--- src/components/cart/CartItem.jsx
+++ src/components/cart/CartItem.jsx
@@ -1,41 +1,51 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
-import Box from '@mui/material/Box';
-import { useGlobalContext } from '../../context';
-//cannot distructure id of tiem
-const CartItem = ({ item, addToCart }) => {
-  const { remove } = useGlobalContext();
-  //need to pull in global context stuff {increase, decrease, toggleAmount}
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
 
-  const { id, title, price, amount, image } = item;
+import { useApi } from '../../hooks/useApi';
 
+const CartItem = ({ id }) => {
+  const [cartItem, setCartItem] = useState('');
+  const request = useApi();
+  const getProduct = async () => {
+    try {
+      const response = await request.get(`products/${id}`);
+      setCartItem(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getProduct();
+  }, [id]);
   return (
-    <Box sx={{ flexGrow: 1 }} position="sticky" style={{ marginTop: 100 }}>
-      <div>
-        <div>
-          <h3>{title}</h3>
-          <div className="information">
-            <p>Price: ${price}</p>
-          </div>
-          <div className="buttons">
-            <Button size="small" disableElevation variant="contained" onClick={() => remove(id)}>
-              -
-            </Button>
-            <p>{amount}</p>
-            <Button
-              size="small"
-              disableElevation
-              variant="contained"
-              onClick={() => addToCart(item)}
-            >
-              +
-            </Button>
-          </div>
-        </div>
-        <img src={image} alt={title} />
-      </div>
-    </Box>
+    <Grid item xs={3}>
+      <Card style={{ maxWidth: '100%' }}>
+        <CardMedia
+          component="img"
+          alt={cartItem.title}
+          image={cartItem.image}
+          style={{ height: 100 }}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {cartItem.title}${cartItem.price}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {cartItem.description}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">{cartItem.category}</Button>
+        </CardActions>
+      </Card>
+    </Grid>
   );
 };
-
 export default CartItem;
